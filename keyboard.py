2c90828f92739ddd2eb00b2d50e34e302e022b4e
Archi <94532438+Champloot@users.noreply.github.com>
2024-10-18 15:06:08 +0300

    Update keyboard.py
    update

--- keyboard.py
+++ keyboard.py
@@ -2,8 +2,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 parse_button = KeyboardButton("/parse")
 start_button = KeyboardButton("/start")
-lol_button = KeyboardButton('LOL')
-all_parse_buttons = [KeyboardButton("LOL")]
+cancel_button = KeyboardButton("/cancel")
+all_parse_buttons = KeyboardButton("LOL_Graphs")
 LolGraphs_buttons = [KeyboardButton("Paranoic"), KeyboardButton("SVIN"), KeyboardButton("Ru"), KeyboardButton("Pyke"),
                      [KeyboardButton("All"),
                       KeyboardButton("Rank"),
@@ -14,17 +14,15 @@ LolGraphs_buttons = [KeyboardButton("Paranoic"), KeyboardButton("SVIN"), Keyboar
                       KeyboardButton("Winrate"),
                       KeyboardButton("KDA")]]
 
-kb_LolGraphs_1_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True).add(LolGraphs_buttons[0])
-kb_LolGraphs_2_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True).add(LolGraphs_buttons[1])
-kb_LolGraphs_3_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True).add(LolGraphs_buttons[2])
-kb_LolGraphs_4_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True).add(LolGraphs_buttons[3])
-kb_LolGraphs_5_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True)
+kb_LolGraphs_1_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True).add(LolGraphs_buttons[0]).add(cancel_button)
+kb_LolGraphs_2_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True).add(LolGraphs_buttons[1]).add(cancel_button)
+kb_LolGraphs_3_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True).add(LolGraphs_buttons[2]).add(cancel_button)
+kb_LolGraphs_4_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True).add(LolGraphs_buttons[3]).add(cancel_button)
+kb_LolGraphs_5_step_FSMParse = ReplyKeyboardMarkup(resize_keyboard=True).add(cancel_button)
 for i in LolGraphs_buttons[4]:
- kb_LolGraphs_5_step_FSMParse.add(i)
-
-
+    kb_LolGraphs_5_step_FSMParse.add(i)
 
 all_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 all_kb.add(parse_button).add(start_button)
 
-gamse_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(lol_button)
+games_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(all_parse_buttons)
